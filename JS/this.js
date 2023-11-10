07b1a6d950261f0fd52a6ca6a8532e738ba53990
MayzMyers <35607480+MayzMyers@users.noreply.github.com>
2023-11-10 15:08:43 +0300

    Решены две задачи

--- JS/this.js
+++ JS/this.js
@@ -1,6 +1,46 @@
-function task1() {}
+function task1() {
+  function getAdd(category) {
+    return function (cod) {
+      return `http://myshop.ru/${category}/${cod}`;
+    };
+  }
+  let createPCLink = getAdd(`pc`);
+  let createNotebooksLink = getAdd(`notebooks`);
+  let createMonitorLink = getAdd(`monitor`);
+  console.log(createPCLink(1001));
+  console.log(createNotebooksLink(4004));
+  console.log(createMonitorLink(345));
+  ˇ;
+}
 
-function task2() {}
+function task2() {
+  let orders = [
+    {
+      fio: "Ivan",
+      tovar: "phone",
+      price: 10000,
+      quantity: 2,
+    },
+    {
+      fio: "Peter",
+      tovar: "usb",
+      price: 2300,
+      quantity: 5,
+    },
+    {
+      fio: "Mary",
+      tovar: "connector",
+      price: 100,
+      quantity: 100,
+    },
+  ];
+  function getOrder(fio, price, quantity) {
+    console.log(
+      `Заказ покупателя ${this.fio} составил ${this.price * this.quantity}`
+    );
+  }
+  orders.forEach((order) => getOrder.call(order));
+}
 
 function task3() {}
 
